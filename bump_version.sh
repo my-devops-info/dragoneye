15963781373803ad0676b48a39f0c88f0d60af04
Tomer Vainshtok <tomerv@indeni.com>
2021-04-26 17:43:19 +0300

    bump version now also works on version.py, added --version for cli

---
+++ bump_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+version_file="dragoneye/version.py"
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git fetch --tags
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "latest tag: $latest_tag"
+echo -e "__version__ = '$latest_tag'\n" > $version_file
+
+git commit --reuse-message=HEAD $version_file || echo "No changes to commit"
+git push origin
